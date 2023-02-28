3a8354bbfd0c09b0eb6cda223085cc1e4f1089eb
Aniket Nigam <aniknigam2@gmail.com>
2023-02-28 14:24:25 +0530

    arethmetic and comparision operatore code done

---
+++ day4_operators_30_days_of_js/day4_operators_30_days_of_js.js
@@ -0,0 +1,69 @@
+//Arithmetic Operators in js
+
+var a = 10;
+var b = 20;
+
+//addition
+var c = a+b;
+document.write(c);
+document.write("<br>") //for breaking the line
+
+//substraction
+var d = a-b;
+document.write(d);
+document.write("<br>") //for breaking the line
+
+//multiplication
+var e = a*b;
+document.write(e);
+document.write("<br>") //for breaking the line
+
+//for division
+var f = a/b;
+document.write(f);
+document.write("<br>") //for breaking the line
+
+//for modulus (remainder)
+var g = a%b;
+document.write(g);
+document.write("<br>") //for breaking the line
+
+//for increment 
+var h = 10;
+ h++;
+document.write(h);
+document.write("<br>") //for breaking the line
+
+//for decrement 
+var i= 10;
+ i--;
+document.write(i);
+document.write("<br>") //for breaking the line
+
+
+//Javascript comparsion operators
+//Comparison operators compare two values and return a boolean value, either true or false. 
+
+// equal operator
+document.write(2 == 2); // true
+document.write("<br>") //for breaking the line
+document.write(2 == '2'); // true
+document.write("<br>") //for breaking the line
+
+// not equal operator
+document.write(3 != 2); // true
+document.write("<br>") //for breaking the line
+document.write('hello' != 'Hello'); // true
+document.write("<br>") //for breaking the line
+
+// strict equal operator
+document.write(2 === 2); // true
+document.write("<br>") //for breaking the line
+document.write(2 === '2'); // false
+document.write("<br>") //for breaking the line
+
+// strict not equal operator
+document.write(2 !== '2'); // true
+document.write("<br>") //for breaking the line
+document.write(2 !== 2); // false
+document.write("<br>") //for breaking the line
