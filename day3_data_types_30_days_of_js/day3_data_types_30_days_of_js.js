ae5a583b703bde73c8e5e77441e3cba96f9c812e
Aniket Nigam <aniknigam2@gmail.com>
2023-02-28 12:46:33 +0530

    Day 3 include data types in js

---
+++ day3_data_types_30_days_of_js/day3_data_types_30_days_of_js.js
@@ -0,0 +1,43 @@
+/* Javascript contain many data types that holds different values.
+There are two types of data types in Javascript
+
+1. Primitive Data type
+2. Non Primitive Data type
+
+In js you don't need to specify type of variable because it is a dynamically used by js engine. you need to use var here to specify the data types. It can contain any type of value like number, strings , etc.
+
+For example
+var a = 40;
+var b = "Hello everyone this is the 30 day of js course"
+
+
+Primitive Data Type : It includes strings, Numbers, boolean, Undefined, null
+
+Non Primitive  Data Types: It include Object, Array, RegExp(represent regular expression)
+
+*/
+
+
+//example of number data type
+ var a = 10; 
+
+// example of string data type
+var b = "hello"
+
+//example of array data types
+var c = ["car", "bus", "train"];
+
+//example of object data type
+var d = {     
+    firstName: "Aniket", 
+    lastNmae: "Nigam",
+    age: 19,
+};
+
+//example of regular expression
+var pattern = /learn js/i;
+
+//example of boolean expression
+var x = true;
+var y = false;
+
