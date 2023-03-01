d686fa33471c9e6d8dc49ddff7834ae059d5801c
Aniket Nigam <aniknigam2@gmail.com>
2023-03-01 13:06:35 +0530

    if statement code done

---
+++ day5_if_else_30_days_of_js/day5_if_else_30_days_of_js.js
@@ -0,0 +1,15 @@
+// If statement
+// It evaluates the content only if expression is true. The signature of JavaScript if statement is given below.
+
+/*
+syntax of if statement
+if(expression){  
+//content to be evaluated  
+}  
+*/
+
+var a = 20;
+if(a>10){
+    document.write("the value of a is greater than 10");
+}
+
