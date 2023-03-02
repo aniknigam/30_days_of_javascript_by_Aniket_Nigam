f6928ce2218426dc69cf2b3a8bfc47c3a571bb50
Aniket Nigam <aniknigam2@gmail.com>
2023-03-02 12:40:04 +0530

    Explaination of what is loop is done

---
+++ day7_loops_30_days_of_js/day7_loops_30_days_of_js.js
@@ -0,0 +1,48 @@
+/*
+Loops are used when you want to write the same piece of code again and again with different values.
+
+
+
+JavaScript supports different kinds of loops:
+
+for - loops through a block of code a number of times
+for-in - loops through the properties of an object
+while - loops through a block of code while a specified condition is true
+do-while - also loops through a block of code while a specified condition is true
+
+
+For example-
+
+Instead of writing:
+
+document.write("1");
+document.write("<br>");
+document.write("2");
+document.write("<br>");
+document.write("3");
+document.write("<br>");
+document.write("4");
+document.write("<br>");
+document.write("5");
+
+we can write:
+
+for(1 = 0; i<0; i++){
+    document.write(i + "<br>")
+}
+
+
+
+
+ */
+for(1 = 1; i<=5; i++){
+    document.write(i + "<br>")
+}
+
+
+for (i=1; i<=5; i++)  
+{  
+document.write(i + "<br/>")  
+}  
+
+document.write("hello")
