70cfc716b1812121beeb765d29a97a02e6b29f84
Aniket Nigam <aniknigam2@gmail.com>
2023-03-09 18:39:38 +0530

    day 11 include objects in javascript

---
+++ day11_objects_30_days_of_js/day11_objects_30_days_of_js.js
@@ -0,0 +1,11 @@
+/*
+JavaScript Objects
+A javaScript object is an entity having state and behavior (properties and method).
+ For example: car, pen, bike, chair, glass, keyboard, monitor etc.
+
+JavaScript is an object-based language. Everything is an object in JavaScript.
+
+JavaScript is template based not class based. Here, we don't create class to get the object. But, we direct create objects.
+
+There are three ways
+*/
